90567f0f10578fa40afa49cab0c9412d735a75bf
Gene Lee <gwlee244@gmail.com>
2020-03-31 21:38:36 -0400

    adding schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+insert into burgers (burger_name)
+values ("Carolina Burger");
+
+insert into burgers (burger_name)
+values ("MCDLT Burger");
+
+insert into burgers (burger_name)
+values ("Incredible Burger");
+
+
+
